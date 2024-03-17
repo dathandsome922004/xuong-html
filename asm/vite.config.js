11d5273ae7b7ea9bc6a3ec377b233823a81427d8
dathandsome92 <150360164+dathandsome92@users.noreply.github.com>
2024-03-17 19:13:32 +0700

    first commit

---
+++ asm/vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        shop: resolve(__dirname, "./webs/shop.html"),
+        view: resolve(__dirname, "./webs/view.html"),
+        cart: resolve(__dirname, "./webs/cart.html"),
+        check_out: resolve(__dirname, "./webs/check_out.html"),
+        lognin: resolve(__dirname, "./webs/lognin.html"),
+      },
+    },
+  },
+});
